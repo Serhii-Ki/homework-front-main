bc35069a7d6d39639492298fa978e92daa8c6b8c
Kiba Sergey <serhiikiba@gmail.com>
2024-04-24 20:33:16 +0200

    HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -13,6 +13,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
+
         <>
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
@@ -31,7 +32,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? s.active : ""
+                        } // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? s.active : ""
+                        } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,12 +52,15 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? s.active : ""
+                        } // делает студент
                     >
                         Junior Plus
                     </NavLink>
                 </nav>
             </aside>
         </>
+
     )
 }
